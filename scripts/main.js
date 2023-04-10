f9a0f68937edcc115aff4b072832fd8b47a57830
Hunter Atkins <lsatkins111@gmail.com>
2023-04-10 12:59:06 -0500

    finished css, beginning inputting card data

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,322 @@
+let cards = [
 
+    {
+        value: 2,
+        file: "images/2_of_clubs.png",
+        aceOrNot: false
+
+    },
+    {
+        value: 2,
+        file: "images/2_of_diamonds.png",
+        aceOrNot: false
+
+    },
+    {
+        value: 2,
+        file: "images/2_of_hearts.png",
+        aceOrNot: false
+
+    },
+    {
+        value: 2,
+        file: "images/2_of_spades.png",
+        aceOrNot: false
+
+    },
+    {
+        value: 3,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 3,
+        file:,
+        aceOrNot: false
+    },
+    {
+        value: 3,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 3,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 4,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 4,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 4,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 4,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 5,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 5,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 5,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 5,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 6,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 6,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 6,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 6,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 7,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 7,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 7,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 7,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 8,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 8,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 8,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 8,
+        file:,
+        aceOrNot: false
+ 
+    },
+    {
+        value: 9,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 9,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 9,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 9,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 10,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 10,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 10,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 10,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 10,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 10,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 10,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 10,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 10,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 10,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 10,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 10,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 10,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 10,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 10,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 10,
+        file:,
+        aceOrNot: false
+
+    },
+    {
+        value: 1,
+        file: "images/ace_of_clubs.png",
+        aceOrNot: true
+
+    },
+    {
+        value: 1,
+        file: "images/ace_of_hearts.png",
+        aceOrNot: true
+
+    },
+    {
+        value: 1,
+        file: "images/ace_of_hearts.png",
+        aceOrNot: true
+
+    },
+    {
+        value: 1,
+        file: "images/ace_of_spades.png",
+        aceOrNot: true
+
+    }
+]
+
+//? code to add cards to hand
+
+// let newCard = document.createElement('img');
+// newCard.setAttribute('class', 'card');
+// newCard.setAttribute('src', "images/ace_of_spades.png")
+// let dealerHand = document.querySelector('#dealer-hand');
+// dealerHand.append(newCard);
